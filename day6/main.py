b583ae97a9f3ad4fdd643c1bd9376b48836c1bce
zaypen <zaypen@gmail.com>
2020-09-14 04:30:31 +0800

    Solve puzzle 6 part 2

--- day6/main.py
+++ day6/main.py
@@ -1,13 +1,22 @@
+from itertools import dropwhile, zip_longest
+
+
 def process(lines):
-    mappings = {satellite: parent for (parent, satellite) in [line.split(')') for line in lines]}
+    mappings = {satellite: parent for (parent, satellite) in [line.split(')') for line in lines if len(line)]}
 
     def orbits_of(satellite):
         parent = mappings.get(satellite)
         if parent:
-            return 1 + orbits_of(parent)
-        return 0
+            return [satellite] + orbits_of(parent)
+        return []
 
-    return sum([orbits_of(satellite) for satellite in mappings.keys()])
+    orbits_of_you = orbits_of('YOU')
+    orbits_of_santa = orbits_of('SAN')
+
+    paths = dropwhile(lambda pair: pair[0] == pair[1], zip_longest(reversed(orbits_of_you), reversed(orbits_of_santa)))
+    path1, path2 = map(lambda path: filter(lambda v: v, path), zip(*paths))
+    transfer_path = list(path1) + ['SHARED'] + list(reversed(list(path2)))
+    return len(transfer_path) - 3
 
 
 def main():
